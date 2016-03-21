a3d66c2209b1695569fbee41430edc1d6f0e673e
genne5 <genne@gmx.net>
2016-03-21 18:35:29 +0100

    Added files via upload

---
+++ WcfBehaviorExtensions/WcfBehaviorExtensions/NewtonsoftJsonBehaviorExtension.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ServiceModel.Configuration;
+
+namespace WcfBehaviorExtensions
+{
+    public class NewtonsoftJsonBehaviorExtension : BehaviorExtensionElement
+    {
+        public override Type BehaviorType
+        {
+            get { return typeof(NewtonsoftJsonBehavior); }
+        }
+
+        protected override object CreateBehavior()
+        {
+            return new NewtonsoftJsonBehavior();
+        }
+    }
+}
